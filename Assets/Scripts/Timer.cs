d4fc0cef9599b257dd694751a98407270f3d466f
NecoZero <artemklemonow@gmail.com>
2023-08-10 19:55:30 +0500

    firstCommit

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    Image timerDar;
+    public float maTime = 20F;
+    float timeLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerDar = GetComponent<Image>();
+        timeLeft = maTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerDar.fillAmount = timeLeft / maTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            Debug.Log("Молодец");
+        }
+    }
+}
